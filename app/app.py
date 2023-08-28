9732c9bc02b3a22d5c27b640ad8e71905716f798
Noppawee Teeraratchanon <earthhnopp@gmail.com>
2023-08-28 21:12:53 +0700

    update some files

--- app/app.py
+++ app/app.py
@@ -10,16 +10,14 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # load the model
 loaded_model = pickle.load(open('/root/code/selling_price.model', 'rb'))
 
-# make a list of brand
-list = ['Ambassador', 'Ashok', 'Audi', 'BMW', 'Chevrolet', 'Daewoo',
-       'Datsun', 'Fiat', 'Force', 'Ford', 'Honda', 'Hyundai', 'Isuzu',
-       'Jaguar', 'Jeep', 'Kia', 'Land', 'Lexus', 'MG', 'Mahindra',
-       'Maruti', 'Mercedes-Benz', 'Mitsubishi', 'Nissan', 'Opel',
-       'Peugeot', 'Renault', 'Skoda', 'Tata', 'Toyota', 'Volkswagen',
-       'Volvo', 'None']
-
 # make a list of owner
 list_owner = ["First Owner", "Second Owner", "Third Owner", "Fourth & Above Owner", "None"]
+
+# make a list of transmission
+list_transmission = ["Automatic", "Manual", "None"]
+
+# make a list of fuel
+list_fuel = ["Diesel", "Petrol", "None"]
 
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
@@ -27,29 +25,37 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.H1("Predicting Car Price System", style={'textAlign': 'center'}),
     html.Br(),
-    html.H6("Instruction", style={'color':'Blue'}),
-    html.H6("The system needs only 5 values to predict the selling car price including:", style={'color':'Blue', 'margin-left': '50px'}),
-    html.H6("1. Brand: User assign the car brand. if you are not sure about the car brand or cannot find your car brand in the list, you can leave it blank or choose None.", style={'color':'Blue', 'margin-left': '50px'}),
-    html.H6("2. Year: Users assign the car age. if you are not sure about the age, you can leave it blank.", style={'color':'Blue', 'margin-left': '50px'}),
-    html.H6("3. Owner: Users assign an ordering owner in the car. if you are not sure about the car ordering owner,  you can leave it blank or choose None.", style={'color':'Blue', 'margin-left': '50px'}),
-    html.H6("4. Mileage: Users assign the mileage of the car. if you are not sure about the mileage of the car, you can leave it blank.", style={'color':'Blue', 'margin-left': '50px'}),
-    html.H6("5. Max power: Users assign the max power of the car. if you are not sure about max power of the car, you can leave it blank.", style={'color':'Blue', 'margin-left': '50px'}),
-    html.H6("After you fill in all of information that you know, you click the submit button, and the website will show the selling price of this car at the bottom of the page.", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("Instruction", style={'color':'Blue'}),
+    html.P("The system needs only 7 values to predict the selling car price including:", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("1. Max power: Users assign the max power of the car. If you are not sure about max power of the car, you can leave it blank.", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("2. Mileage: Users assign the mileage of the car. If you are not sure about the mileage of the car, you can leave it blank.", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("3. Owner: Chaky company sell only the new car which is first hand. so I assign the system to set the first hand as default value", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("4. Km_driven: Chaky company sell only the new car which mean the kilometer driven need to be 0. so I assign the system to set 0 as default value", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("5. Transmission: Users assign the type of transmission of the car. If you are not sure about the transmission type of the car, you can leave it blank or choose None.", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("6. Seats: Users assign the seat number of the car. If you are not sure about the seat number of the car, you can leave it blank.", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("7. Fuel: Users assign the type of fuel that the car use. If you are not sure about the transmission type of the car, you can leave it blank or choose None.", style={'color':'Blue', 'margin-left': '50px'}),
+    html.P("After you fill in all of information that you know, you click the submit button, and the website will show the selling price of this car at the bottom of the page.", style={'color':'Blue', 'margin-left': '50px'}),
     html.Br(),
-    html.H6("Brand : ",  style={'display':'inline-block', 'margin-left': '300px'}),
-    dcc.Dropdown(id='input-brand-state', options=[{'label': list[i], 'value': i} for i in range(len(list))], style={'display':'inline-block','width':'50%'}),
-    html.Br(),
-    html.H6("Year : ",  style={'display':'inline-block', 'margin-left': '300px'}),
-    dcc.Input(id='input-year-state', type='number',min='0'),
-    html.Br(),
-    html.H6("Owner : ",  style={'display':'inline-block', 'margin-left': '300px'}),
-    dcc.Dropdown(id='input-owner-state', options=[{'label': list_owner[i], 'value': i+1} for i in range(len(list_owner))], style={'display':'inline-block','width':'50%'}),
+    html.H6("Max Power : ",  style={'display':'inline-block', 'margin-left': '300px'}),
+    dcc.Input(id='input-max_power-state', type='number'),
     html.Br(),
     html.H6("Mileage : ",  style={'display':'inline-block', 'margin-left': '300px'}),
     dcc.Input(id='input-mileage-state', type='number'),
     html.Br(),
-    html.H6("Max Power : ",  style={'display':'inline-block', 'margin-left': '300px'}),
-    dcc.Input(id='input-max_power-state', type='number'),
+    html.H6("Owner : ",  style={'display':'inline-block', 'margin-left': '300px'}),
+    dcc.Dropdown(id='input-owner-state', options=[{'label': list_owner[i], 'value': i+1} for i in range(len(list_owner))], style={'display':'inline-block','width':'50%'}, value=1 ),
+    html.Br(),
+    html.H6("km_driven : ",  style={'display':'inline-block', 'margin-left': '300px'}),
+    dcc.Input(id='input-km_driven-state', type='number', value=0),
+    html.Br(),
+    html.H6("transmission : ",  style={'display':'inline-block', 'margin-left': '300px'}),
+    dcc.Dropdown(id='input-transmission-state', options=[{'label': list_transmission[i], 'value': i} for i in range(len(list_transmission))], style={'display':'inline-block','width':'50%'}),
+    html.Br(),
+    html.H6("seats : ",  style={'display':'inline-block', 'margin-left': '300px'}),
+    dcc.Input(id='input-seats-state', type='number'),
+    html.Br(),
+    html.H6("fuel : ",  style={'display':'inline-block', 'margin-left': '300px'}),
+    dcc.Dropdown(id='input-fuel-state', options=[{'label': list_fuel[i], 'value': i} for i in range(len(list_fuel))], style={'display':'inline-block','width':'50%'}),
     html.Br(),
     html.Br(),
     html.Button(id='submit-button-state', n_clicks=0, children='Submit', style={'width': '200px', 'margin-left': '625px', 'color': 'Red', 'background': 'White'}),
@@ -57,36 +63,41 @@ app.layout = html.Div([
     html.Br(),
     html.Br(),
     html.H4(id='output-selling_price', style={'color':'Blue', 'margin-left': '50px'})
-], style = { 'background': 'Silver'})
+    ], style = { 'background': 'Silver'})
 
 
 
 @callback(Output('output-selling_price', 'children'),
               Input('submit-button-state', 'n_clicks'),
-              State('input-brand-state', 'value'),
-              State('input-year-state', 'value'),
-              State('input-owner-state', 'value'),
-              State('input-mileage-state', 'value'),
               State('input-max_power-state', 'value'),
+              State('input-mileage-state', 'value'),
+              State('input-owner-state', 'value'),
+              State('input-km_driven-state', 'value'),
+              State('input-transmission-state', 'value'),
+              State('input-seats-state', 'value'),
+              State('input-fuel-state', 'value')
 )
               
-# create output function
-def update_output(n_clicks, brand, year,owner, mileage, max_power):  
+# create output function to predict the selling price of a car
+def update_output(n_clicks,max_power,mileage,owner, km_driven, transmission, seats, fuel):  
     
     if n_clicks>=1:
-        if year == None:
-            year = 2015      #default of year is median of training data which is 2015
-        if mileage == None:
-            mileage = 19.38  #default of mileage is mean of training data which is 19.38
         if max_power == None:
             max_power = 82.85 #default of max power is median of training data which is 82.85
-        if brand == None or brand == 32:
-            brand = 20    #default of brand is mode of training data which is 20 (Maruti)
+        if mileage == None:
+            mileage = 19.38  #default of mileage is mean of training data which is 19.38
         if owner == None or owner == 5:
             owner = 1     #default of owner is mode of training data which is 1 (First Owner)
-        sample = np.array([[max_power, mileage, year, brand, owner]])
+        if km_driven == None:
+            km_driven = 60000   #default of km_driven is median of training data which is 60000 (First Owner)
+        if transmission == None or transmission == 2:
+            transmission = 1    #default of transmission is mode of training data which is 1 (Manual)
+        if seats == None:
+            seats = 5        #default of seats is median of training data which is 5
+        if fuel == None or fuel ==2:
+            fuel = 0        #default of fuel is mode of training data which is 0 (Diesel)
+        sample = np.array([[max_power,mileage,owner, km_driven, transmission, seats, fuel]])
         return f'The selling price of this car = {int(np.exp(loaded_model.predict(sample)))} baht'
-        
 
 if __name__ == '__main__':
     app.run(debug=True)
